1f830e5394c9efde00b1e8aad3cfab991ba30af3
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-12-06 13:03:02 +0530

    string

---
+++ Python/core/string/main.py
@@ -0,0 +1,61 @@
+"""
+what is string in python?
+
+A string is a sequence of characters. In Python, strings are immutable, meaning they cannot be changed after they are created.
+
+"""
+
+# syntax:
+
+# string_name = "Hello, World!"
+# string_name = 'Hello, World!'
+# string_name = '''Hello, World!'''
+string_name = """Hello, World!"""
+# print(type(string_name))
+
+# name = "python tech"
+# print(len(name))
+
+
+# Access elements of the string:
+
+name = "python"
+
+# print(name)
+
+# indexing (+/-)
+
+# print(name[0]) # p
+# print(name[-3]) # h
+
+# slicing (start:stop-1:step-1) (+/-)
+
+# print(name[:])
+
+# print(name[1:])
+
+# print(name[:3])
+
+# print(name[1:4])
+
+# concat
+
+# fname = "brijesh"
+# lname = "gondaliya"
+# fullname = fname + " " + lname
+# print(fullname)
+
+#replica
+# star = "*"
+# print(star*4)
+
+# escape sequence
+# print("brijesh gondaliya")
+# print("\tbrijesh go\ndaliya")
+# print(r"\tbrijesh go\ndaliya")
+
+# sentance = "my name is \"brijesh gondaliya\""
+# sentance = "my name is \'brijesh gondaliya\'"
+
+# sentance = "\\\\"
+# print(sentance)
